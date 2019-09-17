f8cc1f29a2c8e279595522920ad6e6d96e476826
Lyuda Posivnich <nektorandom@gmail.com>
2019-09-17 15:13:19 +0300

    add settings

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -10,8 +10,16 @@ import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
 import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
 import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
+import Font from '@ckeditor/ckeditor5-font/src/font';
+import FontSize from '@ckeditor/ckeditor5-font/src/fontsize';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
+import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough';
+import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline';
+import Subscript from '@ckeditor/ckeditor5-basic-styles/src/subscript';
+import Superscript from '@ckeditor/ckeditor5-basic-styles/src/superscript';
+import Code from '@ckeditor/ckeditor5-basic-styles/src/code';
+import Highlight from '@ckeditor/ckeditor5-highlight/src/highlight';
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
 import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
 import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
@@ -21,6 +29,7 @@ import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
 import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
 import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
 import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
+import Imageresize from '@ckeditor/ckeditor5-image/src/imageresize';
 import Link from '@ckeditor/ckeditor5-link/src/link';
 import List from '@ckeditor/ckeditor5-list/src/list';
 import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed';
@@ -37,8 +46,16 @@ ClassicEditor.builtinPlugins = [
 	UploadAdapter,
 	Alignment,
 	Autoformat,
+	Font,
+	FontSize,
 	Bold,
 	Italic,
+	Code,
+	Strikethrough,
+	Underline,
+	Subscript,
+	Superscript,
+	Highlight,
 	BlockQuote,
 	CKFinder,
 	EasyImage,
@@ -48,6 +65,7 @@ ClassicEditor.builtinPlugins = [
 	ImageStyle,
 	ImageToolbar,
 	ImageUpload,
+	Imageresize,
 	Link,
 	List,
 	MediaEmbed,
@@ -63,18 +81,103 @@ ClassicEditor.defaultConfig = {
 		items: [
 			'heading',
 			'|',
+			'fontFamily',
+			'fontSize',
+			'fontColor',
 			'bold',
 			'italic',
+			'underline',
+			'strikethrough',
+			'subscript',
+			'superscript',
+			'code',
+			'highlight',
 			'alignment',
 			'link',
 			'bulletedList',
 			'numberedList',
-			'imageUpload',
 			'blockQuote',
+			'|',
 			'insertTable',
+			'imageUpload',
 			'mediaEmbed',
 			'undo',
 			'redo'
+		]
+	},
+	fontFamily: {
+		options: [
+			'RobotRegular, sans-serif',
+			'RobotLight, sans-serif',
+			'RobotMedium, sans-serif',
+			'RobotBold, sans-serif',
+			'default',
+			'Arial, Helvetica, sans-serif',
+			'Courier New, Courier, monospace',
+			'Georgia, serif',
+			'Lucida Sans Unicode, Lucida Grande, sans-serif',
+			'Tahoma, Geneva, sans-serif',
+			'Times New Roman, Times, serif',
+			'Trebuchet MS, Helvetica, sans-serif',
+			'Verdana, Geneva, sans-serif'
+		]
+	},
+	fontSize: {
+		options: [
+			'tiny',
+			'small',
+			'default',
+			'big',
+			'huge'
+		],
+	},
+	fontColor: {
+		colors: [
+			{
+				color: '#2A2117',
+				label: 'Main'
+			},
+			{
+				color: '#94897C',
+				label: 'Second'
+			},
+			{
+				color: '#D0C5B9',
+				label: 'Third'
+			},
+			{
+				color: '#EDE4DA',
+				label: 'Background'
+			},
+			{
+				color: '#AB0000',
+				label: 'Red'
+			},
+			{
+				color: '#37AB00',
+				label: 'Green'
+			},
+			{
+				color: 'hsl(0, 0%, 0%)',
+				label: 'Black'
+			},
+			{
+				color: 'hsl(0, 0%, 30%)',
+				label: 'Dim grey'
+			},
+			{
+				color: 'hsl(0, 0%, 60%)',
+				label: 'Grey'
+			},
+			{
+				color: 'hsl(0, 0%, 90%)',
+				label: 'Light grey'
+			},
+			{
+				color: 'hsl(0, 0%, 100%)',
+				label: 'White',
+				hasBorder: true
+			},
 		]
 	},
 	alignment: {
@@ -83,10 +186,21 @@ ClassicEditor.defaultConfig = {
 	image: {
 		toolbar: [
 			'imageStyle:full',
+			'imageStyle:alignLeft',
+			'imageStyle:alignCenter',
+			'imageStyle:alignRight',
 			'imageStyle:side',
 			'|',
 			'imageTextAlternative'
-		]
+		],
+		styles: [
+			'full',
+			'alignLeft',
+			'alignCenter',
+			'alignRight',
+			'side',
+		],
+		resizeUnit: 'px'
 	},
 	table: {
 		contentToolbar: [
